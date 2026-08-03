8d338a4cf0b270c0474cbac1db04aa6d226d8233
Ossama Laaraj <oussamalaarge24@gmail.com>
2026-08-03 02:49:08 +0100

    pesse

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ function nowHM() {
 
 function computeDateProduction() {
   const now = new Date();
-  if (now.getHours() < 7) now.setDate(now.getDate() - 1);
+  if (now.getHours() < 6) now.setDate(now.getDate() - 1);
   return now.toISOString().slice(0, 10);
 }
 
